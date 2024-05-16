bfdf63d88654fe2464875dcc4a775218d0d8bd6e
Rohit <singhrohit51967@gmail.com>
2024-05-16 08:32:06 +0530

    subtodo schema

--- 03_DataModelling/sub_todo.models.js
+++ 03_DataModelling/sub_todo.models.js
@@ -1,7 +1,20 @@
 import mongoose from 'mongoose'
 
 const subTodoSchema = new mongoose.Schema(
-    {},
+    {
+        content: {
+            type: String,
+            required: true,
+        },
+        complete: {
+            type: Boolean,
+            default: false,
+        },
+        createdBy: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+        }
+    },
     {
         timestamps: true,
     }

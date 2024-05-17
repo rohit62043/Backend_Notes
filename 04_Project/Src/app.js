d4ae411748eb775d36c494b6b64282405dfde03e
Rohit <singhrohit51967@gmail.com>
2024-05-17 11:18:24 +0530

    setting up basic middlewares

---
+++ 04_Project/Src/app.js
@@ -0,0 +1,25 @@
+import express from 'express'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+
+const app = express();
+
+// Enable CORS with specific origin and credentials support
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,// Allows only the specified origin (set in the environment variable CORS_ORIGIN) to access the server.
+    credentials: true// Allows cookies and other credentials to be included in requests to the server.
+}))
+// Parse incoming JSON requests with a size limit of 16kb
+app.use(express.json({ limit: "16kb" }))
+
+// Parse incoming URL-encoded requests with a size limit of 16kb
+app.use(express.urlencoded({
+    extended: true//allows for rich objects and arrays to be encoded into the URL-encoded format.
+    , limit: "16kb"
+}))
+app.use(express.static("public"))//Serves static files (like HTML, CSS, JS, images) from the public directory.
+
+app.use(cookieParser());//Parses cookies attached to client requests and makes them accessible via req.cookies.
+
+
+export { app }
--- 04_Project/Src/index.js
+++ 04_Project/Src/index.js
@@ -1,7 +1,14 @@
 // require('dotenv').config({ path: './env' })
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
-connectDB();
+connectDB().then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port : ${process.env.PORT}`)
+    })
+}).catch((err) => {
+    console.log("Mongo DB conection failed!!!", err)
+});
 
 
 // console.log(process.env.MONGODB_URI)

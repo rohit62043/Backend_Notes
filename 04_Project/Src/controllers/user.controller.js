a9c4aadd802a02392edacfc9940b76dd2fe2b4e3
Rohit <singhrohit51967@gmail.com>
2024-06-09 13:54:43 +0530

    controller for updating user avatar and coverImage

--- 04_Project/Src/controllers/user.controller.js
+++ 04_Project/Src/controllers/user.controller.js
@@ -236,22 +236,61 @@ const updateAccountDetails = asyncHandler(async (req, res) => {
             }
         },
         { new: true }
-    ).select("-refreshToken -password");
+    ).select("-password");
 
     return res.status(200).json(new ApiResponse(200, user, "Account details Successfully."))
 })
 
+//Note:Further update while updating file we cn delete the preious file from clodinary
 const updateUserAvatar = asyncHandler(async (req, res) => {
     const avatarLocalPath = req.file?.path
     if (!avatarLocalPath) {
         throw new ApiError(400, "Avatar file is missing.");
     }
-
+    //TODO: delete old image - assignment
     const avatar = await uploadOnCloudinary(avatarLocalPath)
 
     if (!avatar?.url) {
         throw new ApiError(400, "Error while uploading avatar on cloudinary")
     }
+
+    const user = await User.findByIdAndUpdate(req.user?._id,
+        {
+            $set: {
+                avatar: avatar.url
+            },
+        },
+        {
+            new: true
+        }
+    ).select("-password")
+
+    return res.status(200).json(200, user, "Avatar Image Updated Successfully.")
+})
+const updateUserCoverImage = asyncHandler(async (req, res) => {
+    const coverImageLocalPath = req.file?.path
+    if (!coverImageLocalPath) {
+        throw new ApiError(400, "Avatar file is missing.");
+    }
+    //TODO: delete old image - assignment
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+    if (!coverImage?.url) {
+        throw new ApiError(400, "Error while uploading coverImage on cloudinary")
+    }
+
+    const user = await User.findByIdAndUpdate(req.user?._id,
+        {
+            $set: {
+                coverImage: coverImage.url
+            },
+        },
+        {
+            new: true
+        }
+    ).select("-password")
+
+    return res.status(200).json(200, user, "coverImage Image Updated Successfully.")
 })
 
-export { registerUser, loginUser, logoutUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails }
+export { registerUser, loginUser, logoutUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage }
